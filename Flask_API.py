74576244aff0219e90a18e09d420b99486ec3892
Ophelia Heard <opheliaheard@Ophelias-MBP.attlocal.net>
2021-06-29 18:36:47 -0500

    set db and import dependencies in Flask file

--- Flask_API.py
+++ Flask_API.py
@@ -1,9 +1,20 @@
-# 1. import Flask
-from flask import Flask
-# from test_function import *
+# Import dependencies
+from flask import Flask, request, redirect, jsonify
+import numpy as np 
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
+
+# SET UP DATABASE & DB REFERENCES
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # 3. Define what to do when a user hits the index route
 @app.route("/")
